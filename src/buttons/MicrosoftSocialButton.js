86ababf1bf7bd841d18ebf90a240ea885968eb1a
Dan Clipca <virtumonde@virtumonde.local>
2019-10-26 22:47:13 -0400

    Second import

--- src/buttons/MicrosoftSocialButton.js
+++ src/buttons/MicrosoftSocialButton.js
@@ -3,15 +3,15 @@ import { Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 
 const styles = StyleSheet.create({
   microsoftStyle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#0078d7',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#0078d7",
     borderWidth: 0.5,
-    borderColor: '#fff',
+    borderColor: "#fff",
     height: 40,
     width: 220,
     borderRadius: 5,
-    margin: 5,
+    margin: 5
   },
   imageIconStyle: {
     padding: 10,
@@ -24,24 +24,26 @@ const styles = StyleSheet.create({
     color: "#ffffff",
     marginLeft: 15,
     marginRight: 20
-  },
+  }
 });
 
 export class MicrosoftSocialButton extends React.Component {
-
   render() {
     return (
       <TouchableOpacity
-        style={{...styles.microsoftStyle, ...this.props.style}}
+        style={{ ...styles.microsoftStyle, ...this.props.style }}
         onPress={this.props.onPress}
       >
         <Image
           source={require("../images/microsoft.png")}
           style={styles.imageIconStyle}
         />
-        <Text style={styles.textStyle}>{this.props.buttonText ? this.props.buttonText : "Sign in with Microsoft"}</Text>
+        <Text style={styles.textStyle}>
+          {this.props.buttonText
+            ? this.props.buttonText
+            : "Sign in with Microsoft"}
+        </Text>
       </TouchableOpacity>
     );
   }
 }
-
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import { FacebookSocialButton }  from "../src/buttons/FacebookSocialButton";
-import { AmazonSocialButton }  from "../src/buttons/AmazonSocialButton";
-import { LinkedInSocialButton }  from "../src/buttons/LinkedInSocialButton";
-import { GoogleSocialButton }  from "../src/buttons/GoogleSocialButton";
-import { MicrosoftSocialButton }  from "../src/buttons/MicrosoftSocialButton";
-import { GitHubSocialButton }  from "../src/buttons/GitHubSocialButton";
-import { TwitterSocialButton }  from "../src/buttons/TwitterSocialButton";
-import { InstagramSocialButton }  from "../src/buttons/InstagramSocialButton";
+import { FacebookSocialButton } from "../src/buttons/FacebookSocialButton";
+import { AmazonSocialButton } from "../src/buttons/AmazonSocialButton";
+import { LinkedInSocialButton } from "../src/buttons/LinkedInSocialButton";
+import { GoogleSocialButton } from "../src/buttons/GoogleSocialButton";
+import { MicrosoftSocialButton } from "../src/buttons/MicrosoftSocialButton";
+import { GitHubSocialButton } from "../src/buttons/GitHubSocialButton";
+import { TwitterSocialButton } from "../src/buttons/TwitterSocialButton";
+import { InstagramSocialButton } from "../src/buttons/InstagramSocialButton";
 
-
-
-export { FacebookSocialButton }
-export { AmazonSocialButton }
-export { LinkedInSocialButton }
-export { GoogleSocialButton }
-export { MicrosoftSocialButton }
-export { GitHubSocialButton }
-export { TwitterSocialButton }
-export { InstagramSocialButton }
+export { FacebookSocialButton };
+export { AmazonSocialButton };
+export { LinkedInSocialButton };
+export { GoogleSocialButton };
+export { MicrosoftSocialButton };
+export { GitHubSocialButton };
+export { TwitterSocialButton };
+export { InstagramSocialButton };
