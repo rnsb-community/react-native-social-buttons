630f6940f056d8c5ae320fd1d76467d5fc85f09a
Dan Clipca <47941658+virtumonde@users.noreply.github.com>
2020-07-16 07:03:42 -0400

    Create AppleSocialButton.js

---
+++ src/buttons/AppleSocialButton.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+
+const styles = StyleSheet.create({
+  githubStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333",
+    borderWidth: 0.5,
+    borderColor: "#fff",
+    height: 40,
+    width: 220,
+    borderRadius: 5,
+    margin: 5
+  },
+  imageIconStyle: {
+    padding: 10,
+    marginLeft: 15,
+    height: 25,
+    width: 25,
+    resizeMode: "stretch"
+  },
+  textStyle: {
+    color: "#fff",
+    marginLeft: 20,
+    marginRight: 20
+  }
+});
+
+export class AppleSocialButton extends React.Component {
+  render() {
+    return (
+      <TouchableOpacity
+        style={{ ...styles.githubStyle, ...this.props.buttonViewStyle }}
+        onPress={this.props.onPress}
+      >
+        <Image
+          source={require("../images/apple.png")}
+          style={{...styles.imageIconStyle, ...this.props.logoStyle}}
+        />
+        <Text style={{...styles.textStyle, ...this.props.textStyle}}>
+          {this.props.buttonText
+            ? this.props.buttonText
+            : "Sign in with GitHub"}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
